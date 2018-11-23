423cd9df6bd155291e43494aba256bd3726525bf
kwilso82 <300957902@cencol.ca>
2018-11-22 20:58:58 -0500

    Add project files.

---
+++ GoldieGames/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GoldieGames.Models;
+
+namespace GoldieGames.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Item()
+        {
+            return View();
+        }
+
+        public IActionResult AddItem()
+        {
+            return View();
+        }
+
+        public IActionResult ItemsList()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GoldieGames/Models/BoardGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldieGames.Models
+{
+    public class BoardGame
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Seller { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+    }
+}
