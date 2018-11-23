c0c5388e920058b36e6069f734c497a167d66448
kwilso <45276813+kwilso@users.noreply.github.com>
2018-11-22 21:25:27 -0500

    start fixing

--- GoldieGames/Models/Order.cs
+++ GoldieGames/Models/Order.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace COMP229GroupWork.Models
+namespace GoldieGames.Models
 {
     public class Order
     {
