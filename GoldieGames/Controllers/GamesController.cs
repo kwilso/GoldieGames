6594cc7daae98569fca266970c69cd09d622a4c0
양정민(Austin) <tntnrndh@gmail.com>
2018-11-29 21:56:05 -0500

    changed Item action and ItemList View form

--- GoldieGames/Controllers/GamesController.cs
+++ GoldieGames/Controllers/GamesController.cs
@@ -43,10 +43,10 @@ namespace GoldieGames.Controllers
         }
 
         [HttpPost]
-        public ViewResult Item(string title)
+        public ViewResult Item(int boardgameID)
         {
             var boardgame = repository.BoardGames
-                .FirstOrDefault(p => p.Title == title);
+                .FirstOrDefault(p => p.BoardGameID == boardgameID);
             return View("Item", boardgame);
         }
 
