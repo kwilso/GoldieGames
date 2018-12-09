86defd7acad1b466828d7c74647bf9781d407cd7
Kashawn W Flockavela <Kashawn W Flockavela@KASHAWNSLAPTOP>
2018-12-09 16:49:46 -0500

    All games from home page are in seed data
    Removed old commented code in EFBoard, Login, Item, ItemsList

--- GoldieGames/Models/EFBoardGameRepository.cs
+++ GoldieGames/Models/EFBoardGameRepository.cs
@@ -59,43 +59,5 @@ namespace GoldieGames.Models
             return dbEntry;
         }
 
-        /* private List<BoardGame> boardgames = new List<BoardGame>()
-         {
-             new BoardGame {BoardGameID=1, Name = "GTA V", Price = 49.99M, Description = "This is an open world game" , Category = "Open World"},
-             new BoardGame {BoardGameID=2, Name = "Dragonball Xenoverse", Price = 59.99M, Description = "This is mario game", Category = "Action" },
-             new BoardGame {BoardGameID=3, Name = "Minecraft", Price = 99.99M, Description = "This is an open world game", Category="Open World" }
-         };*/
-        /*public BoardGameRepository()
-        {
-            var initialData = new List<BoardGame>()
-            {
-                new BoardGame {BoardGameID=1, Name = "Chess", Price = 29.99M, Description = "This is a chess board game made of wood" , Category = "Board"},
-            new BoardGame {BoardGameID=2, Name = "UNO", Price = 39.99M, Description = "UNO is a card game you can play with your friends or familiy members!", Category = "Card" },
-            new BoardGame {BoardGameID=3, Name = "Monopoly", Price = 49.99M, Description = "Monopoly is a game you compete other players, buy house and take money from players", Category="Dice roll" } };
-            
-            foreach (BoardGame obj in initialData)
-            {
-                AddGame(obj);
-            }
-        }*/
-
-        /*private static List<BoardGame> _BoardGame = new List<BoardGame>(){
-            new BoardGame {BoardGameID=1, Title = "Chess", Price=29.99M, Seller="Alams", Genre="Family"},
-            new BoardGame {BoardGameID=2, Title = "Monopoly", Price=34.99M, Seller="Hasbro", Genre="Family"},
-            new BoardGame {BoardGameID=3, Title = "UNO", Price=45.99M, Seller="Amigo", Genre="Family"},
-            };
-        public IEnumerable<BoardGame> BoardGames
-        {
-            get
-            {
-                return _BoardGame;
-            }
-        }
-
-        public void AddBoardGame(BoardGame boardgame)
-        {
-            _BoardGame.Add(boardgame);
-        }*/
-
     }
 }
